070b33043ce5abb00db890b7b902de6b7c93771d
ahmed5768 <101082079+ahmed5768@users.noreply.github.com>
2024-05-11 03:41:15 +0500

    done

---
+++ app.js
@@ -0,0 +1,81 @@
+// Task no 01
+var arr = []
+
+// Task no 03
+// for(i = 1 ; i <= 10 ; i++){
+//     document.write(`${i} <br>`);
+// }
+
+// Task no 04
+// var table1 = +prompt("Enter a number")
+// var table2 = +prompt("Enter a number table ending point")
+// for(i = 1 ; i <= table2 ; i++){
+//     document.write(`${table1} * ${i} = ${table1*i} <br>`)
+// }
+
+// Rask no 05
+// var table3 = 2
+// var table4 = 15
+// for(i = 1 ; i <= table4 ; i++){
+//     document.write(`${table3} * ${i} = ${table3*i} <br>`)
+// }
+
+// Task no 06
+// var fruits = ['apple', 'banana', 'mango', 'orange','strawberry']
+// for(i = 0 ; i < fruits.length ; i++){
+//     document.write(`Element at index ${i} is ${fruits[i]} <br>`)
+// }
+
+//Task no 07
+// document.write(`<h1>Counting</h1>`)
+// for(i = 1 ; i <= 10 ; i++){
+//     document.write(` ${i} <br>`)
+// }
+
+// document.write(`<h1>Reverse</h1>`)
+// for(i = 10 ; i >= 1 ; i--){
+//     document.write(` ${i} <br>`)
+// }
+
+// document.write(`<h1>Even</h1>`)
+// for(i = 0 ; i <= 20 ; i++){
+//     if(i%2 == 0){
+//         document.write(` ${i} <br>`)
+//     }else{}
+// }
+
+// document.write(`<h1>Odd</h1>`)
+// for(i = 0 ; i <= 20 ; i++){
+//     if(i%2 == 1){
+//         document.write(` ${i} <br>`)
+//     }else{}
+// }
+
+// document.write(`<h1>Series</h1>`)
+// for(i = 2 ; i <= 20 ; i++){
+//     if(i%2 == 0){
+//         document.write(` ${i}k <br>`)
+//     }else{}
+// }
+
+// Task no 07
+// var A = ['cake', 'apple pie', 'cookie', 'chips', 'patties']
+// var match = false
+// var fruitsPrmt = prompt("Welcome to bakery")
+// for(i = 0 ; i < A.length ; i++){
+//     if(A[i] === fruitsPrmt){
+//         match= true
+//         alert(`${fruitsPrmt} is available at index ${i} in our bakery`)
+//         break
+//     }  
+// }
+// if(match == false){
+//     alert(`We are sorry. ${fruitsPrmt} is not available in our bakery`)
+// }
+
+// Task no 08
+var A = [24, 53, 78, 91, 12]
+var highest = 0
+for (let i = 0; i < A.length; i++) {
+    
+}
